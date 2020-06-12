dea154a169429c7360cea10341da60d97e571aa2
DELL <DELL@DESKTOP-T5G1GT0>
2020-06-12 08:53:02 +0530

    Examplepractice

---
+++ Milestone1/src/com/wipro/languagebasics/Ex1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wipro.languagebasics;
+
+/**
+ * @author DELL
+ *
+ */
+public class Ex1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
