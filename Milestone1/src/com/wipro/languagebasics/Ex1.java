3e220b436c98fa5ce9830c3304a859002bfca36b
DELL <DELL@DESKTOP-T5G1GT0>
2020-06-12 09:33:59 +0530

    Languagebasicsprogramms

--- Milestone1/src/com/wipro/languagebasics/Ex1.java
+++ Milestone1/src/com/wipro/languagebasics/Ex1.java
@@ -13,6 +13,10 @@ public class Ex1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String a=args[0];
+		String b=args[1];
+		System.out.println(a+" Technologies "+b);
+		
 		// TODO Auto-generated method stub
 
 	}
---
+++ Milestone1/src/com/wipro/languagebasics/Ex2.java
@@ -0,0 +1,11 @@
+package com.wipro.languagebasics;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome "+args[0]);
+		// TODO Auto-generated method stub
+
+	}
+
+}
